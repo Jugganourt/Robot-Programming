fa4bf69f588ab5231499305232ec072ee115e447
Jugganourt <t.sammons@hotmail.com>
2014-01-19 15:56:01 +0000

    MazExcape.java

---
+++ MazExcape.java
@@ -0,0 +1,69 @@
+package robotics.inatthedeepend;
+
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.Delay;
+
+public class MazExcape 
+{
+	private final static RegulatedMotor left = Motor.C;
+	private final static RegulatedMotor right = Motor.B;
+	private final static DifferentialPilot pilot = new DifferentialPilot(56, 122, left, right);
+
+	public static void main(String[] args)
+	{
+		UltrasonicSensor range = new UltrasonicSensor(SensorPort.S4);
+		TouchSensor sensor = new TouchSensor(SensorPort.S1);
+		while(Button.ENTER.isUp())
+		{
+			if(range.getRange() < 100 && !sensor.isPressed())	//if no bump and out of range sensor keep driving
+			{
+				drive(); 	//keep driving forward 
+			}
+			else if(range.getRange() < 100 && sensor.isPressed())
+			{
+				bump();
+			}
+			else
+			{
+				pilot.rotate(90);
+			}
+		}
+	}
+	
+	public static void bump()
+	{
+		TouchSensor sensor = new TouchSensor(SensorPort.S1);
+		if(sensor.isPressed()) //if bump
+		{
+			backwards(); //back a bit
+			spin(180); //turn
+		}
+	}
+	
+	private void stop() 
+	{
+		pilot.stop();
+	}
+
+	private static void spin(double angle)
+	{
+		pilot.rotate(angle);;
+	}
+	
+	public static void backwards()
+	{
+		pilot.backward();
+		Delay.msDelay(500);
+	}
+	
+	public static void drive()
+	{
+		pilot.forward();
+	}
+}
