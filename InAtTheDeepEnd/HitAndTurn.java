5bcd524adb44a81c1d60e9eda359111a3286c896
Jugganourt <t.sammons@hotmail.com>
2014-01-23 17:45:43 +0000

    Update HitAndTurn.java

--- InAtTheDeepEnd/HitAndTurn.java
+++ InAtTheDeepEnd/HitAndTurn.java
@@ -1,56 +1,35 @@
 package robotics.inatthedeepend;
 
 import lejos.nxt.Button;
-import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
-import lejos.robotics.RegulatedMotor;
-import lejos.robotics.navigation.DifferentialPilot;
-import lejos.util.Delay;
 
-public class HitAndTurn {
+/**
+ * 	The Robot will hit something and do a 180 spin in the opposite direction
+ * @author txs397
+ */
+public class HitAndTurn 
+{
+	public static void main(String[] args)
+    {
+		RobotInformation robot = new RobotInformation();
+        TouchSensor sensor = new TouchSensor(SensorPort.S1);
+        robot.SetSpeed(200);
         
-        private final static RegulatedMotor left = Motor.C;
-        private final static RegulatedMotor right = Motor.B;
-        private final static DifferentialPilot pilot = new DifferentialPilot(56, 122, left, right);
-        
-        
-        public static void main(String[] args)
-        {
-                TouchSensor sensor = new TouchSensor(SensorPort.S1);
+        System.out.println("To start press The Orange Button");
+        System.out.println("");
+        System.out.println("To STOP hold The Orange Button");
                 
-                Button.waitForAnyPress();
-                Delay.msDelay(500);
-                while(Button.ENTER.isUp()) //loop
-                {
-                        drive();//go forward
-                        if(sensor.isPressed()) //if bump
-                        {
-                                backwards(); //back a bit
-                                spin(180); //turn
-                        }
-                }
-        }
-        
-        private void stop() {
-                pilot.stop();
-        }
-
-        private static void spin(double angle) {
-                pilot.rotate(angle);;
-        }
-        
-        public static void backwards()
+        robot.ButtonPress();
+        while(Button.ENTER.isUp()) //loop
         {
-                pilot.backward();
-                Delay.msDelay(500);
+        	robot.Drive();//go forward
+            
+        	if(sensor.isPressed()) //if bump
+            {
+        		robot.Backwards(); //back a bit
+                robot.Spin(180); //turn
+            }
         }
-        
-        public static void drive()
-        {
-                pilot.forward();
-        }
-
-        
-        
+    }
 }
