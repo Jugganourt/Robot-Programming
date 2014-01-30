2612fe992590ea25bcdeb2f932a08598b88197a5
Jugganourt <t.sammons@hotmail.com>
2014-01-30 16:29:00 +0000

    Update Shapes.java

--- InAtTheDeepEnd/Shapes.java
+++ InAtTheDeepEnd/Shapes.java
@@ -5,29 +5,26 @@ import lejos.util.Delay;
 
 /**
  * makes the robot draw shapes
+ * 
  * @author txs397
  */
-public class Shapes 
-{
-	public static void main(String[] args)
-    {
+public class Shapes {
+	public static void main(String[] args) {
 		RobotInformation robot = new RobotInformation();
-		robot.SetSpeed(200);
-        System.out.println("Hello World!!");
-        System.out.println("");
-        System.out.println("To start press The Orange Button");
-        System.out.println("");
-        System.out.println("To STOP hold The Orange Button");
-              
-        robot.ButtonPress();
-        while(Button.ENTER.isUp())
-        {
-        	robot.Square();
-        }
-        Delay.msDelay(500);
-        while(Button.ENTER.isUp())
-        {
-        	robot.Hexagon();
-        }
-    }
+		robot.setSpeed(200);
+		System.out.println("Hello World!!");
+		System.out.println("");
+		System.out.println("To start press The Orange Button");
+		System.out.println("");
+		System.out.println("To STOP hold The Orange Button");
+
+		robot.buttonPress();
+		while (Button.ENTER.isUp()) {
+			robot.square();
+		}
+		Delay.msDelay(500);
+		while (Button.ENTER.isUp()) {
+			robot.hexagon();
+		}
+	}
 }
