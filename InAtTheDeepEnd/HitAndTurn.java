9a31f2b2c678e4e623c05e5f23c0761b438d4662
Jugganourt <t.sammons@hotmail.com>
2014-01-30 16:28:08 +0000

    Update HitAndTurn.java

--- InAtTheDeepEnd/HitAndTurn.java
+++ InAtTheDeepEnd/HitAndTurn.java
@@ -1,78 +1,34 @@
 package robotics.inatthedeepend;
 
 import lejos.nxt.Button;
-import lejos.nxt.LightSensor;
-import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
-import lejos.nxt.UltrasonicSensor;
-import lejos.util.Delay;
-
 
 /**
- * Made so that the robot can escape a maze
+ * The Robot will hit something and do a 180 spin in the opposite direction
+ * 
  * @author txs397
- *
  */
-public class MazeExcape
-{
-	public static void main(String[] args)
-    {
-		System.out.println("To start press The Orange Button");
-        System.out.println("");
-        System.out.println("To STOP hold The Orange Button");
-        
+public class HitAndTurn {
+	public static void main(String[] args) {
 		RobotInformation robot = new RobotInformation();
-        UltrasonicSensor range = new UltrasonicSensor(SensorPort.S4);
-        TouchSensor sensor = new TouchSensor(SensorPort.S1);
-        
-        
-        robot.ButtonPress();
-        while(Button.ENTER.isUp())
-        {
-        	robot.SetSpeed(200);
-        	range.continuous();
-          	
-        	if(range.getDistance() < 10 )
-        	{
-        		robot.Stop();
-        		Motor.C.forward();
-        		Delay.msDelay(500);
-        		robot.Stop();
-        		Motor.B.forward();
-        		Delay.msDelay(500);
-        		robot.Stop();
-        	}
-        	
-            if(range.getDistance() <= 45 && !sensor.isPressed()) //if left wall is in range and no bump...keep moving forward
-            {
-            	robot.Drive(); 
-            }
-            
-            if(range.getDistance() > 45 && !sensor.isPressed())
-            {	
-            	robot.Stop();
-            	robot.NoWall(); //if wall is out of range.... turn left and continue
-            	robot.Stop();
-            }
+		TouchSensor sensor = new TouchSensor(SensorPort.S1);
+		robot.setSpeed(200);
 
-            if(range.getDistance() <= 45 && sensor.isPressed()) //if left wall is in range but has a bump...turn -90 degrees (to the right)
-            {
-            	robot.Stop();
-            	robot.Backwards();
-            	robot.Backwards();
-                robot.Spin(-90);
-            }
-            
-            if(range.getDistance() > 45 && sensor.isPressed())
-            {	
-            	robot.Stop();
-            	robot.Backwards();
-            	robot.Backwards();
-                robot.Spin(90);
-                robot.Stop();
-            }
-            
-        }
-    }
+		System.out.println("To start press The Orange Button");
+		System.out.println("");
+		System.out.println("To STOP hold The Orange Button");
+
+		robot.buttonPress();
+		while (Button.ENTER.isUp()) // loop
+		{
+			robot.drive();// go forward
+
+			if (sensor.isPressed()) // if bump
+			{
+				robot.backwardsMimimum(); // back a bit
+				robot.spin(180); // turn
+			}
+		}
+	}
 }
