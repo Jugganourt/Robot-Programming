e17d65e3474fe10d23e0c20b4ef21422f2a28f21
Jugganourt <t.sammons@hotmail.com>
2014-02-08 14:59:17 +0000

    Update FollowLine.java

--- FeedbackAndFriends/FollowLine.java
+++ FeedbackAndFriends/FollowLine.java
@@ -1,71 +1,56 @@
+package robotics.FeedbackAndFriend;
+
 import lejos.nxt.Button;
 import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
-import lejos.nxt.comm.RConsole;
 import lejos.util.Delay;
-
 
 public class FollowLine extends RobotSettings {
 
 	public static LightSensor left = new LightSensor(SensorPort.S3);
 	public static LightSensor right = new LightSensor(SensorPort.S2);
-	private int threshold;
-	public FollowLine(int threshold) {
+	private int threshold = 40;
+
+	public FollowLine() {
 		super();
-		this.threshold = threshold;
 	}
 
-
-	public void run(){
-			while(m_run())
+	public void run() {
+		while (m_run()) {
+			pilot.setTravelSpeed(300);
+			
+			if(left.readValue() < threshold && right.readValue() >threshold)
 			{
-				findLine();
+				pilot.rotate(20);
+			}
+			
+			if(left.readValue() < threshold && right.readValue() < threshold)
+			{
+				if(left.readValue( )<threshold){
+					pilot.rotate(-90);
+				}
+				else{
+					pilot.rotate(90);
+				}
+			
+			}
+			if(left.readValue() > threshold && right.readValue() > threshold )
+			{
+				pilot.forward(); 
+			} 
+			if(left.readValue() > threshold && right.readValue() < threshold)
+			{
+				pilot.rotate(-20);
+				
 			}
 		}
-	
+	}
 
 	public static void main(String[] args) {
-			Button.waitForAnyPress();
-			Delay.msDelay(500);
-			FollowLine run = new FollowLine(30);
-			run.run();
-		
-	}
-	
-	public boolean isLineLeft()
-	{
-		if(left.readValue()<threshold)
-			return true;
-		return false;
-	}
-	
-	public boolean isLineRight()
-	{
-		if(right.readValue()<threshold)
-			return true;
-		return false;
-	}
-	
-	
-	
-	public void findLine()
-	{
-		if(isLineRight() == false && isLineLeft() == false)
-		{
-			pilot.travel(10, true);
-		}
-		while(isLineLeft()==true)
-		{	
-			pilot.rotate(5);
-		}
-		while(isLineRight()==true)
-		{	
-			pilot.rotate(-5);
-		}
-		
-		
-		
-		
-	}
+		Button.waitForAnyPress();
+		Delay.msDelay(500);
+		FollowLine run = new FollowLine();
+		run.run();
 
+	}
 }
