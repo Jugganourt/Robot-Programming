d59856d8dd6498f76bf3d2bf26a3c759fe114cc8
Jugganourt <t.sammons@hotmail.com>
2014-02-24 17:33:11 +0000

    urm something i guess

--- SearchingForFun/src/searchingForFun/Searches/BreadthFirstSearch.java
+++ SearchingForFun/src/searchingForFun/Searches/BreadthFirstSearch.java
@@ -1,10 +1,19 @@
 package searchingForFun.Searches;
 
+import rp13.search.problem.puzzle.EightPuzzle;
+
 public class BreadthFirstSearch {
 	
+	public static void main(String[] args){
 	//take the first node
+	
 	//see if its the goal state
+	EightPuzzle goal = EightPuzzle.orderedEightPuzzle();
+	System.out.println(goal);
 	//if not, finds successors
+	
 	//add successors to the back of the list
+	
 	//start again...
+	}
 }
