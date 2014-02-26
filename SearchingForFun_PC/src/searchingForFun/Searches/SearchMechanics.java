8aba7dd5a29db5eab4b4d35945e460c6089ddc97
Jugganourt <t.sammons@hotmail.com>
2014-02-26 00:01:00 +0000

    Search Macshine V1
    to attmept to combine the BFS adn DFT code together

---
+++ SearchingForFun_PC/src/searchingForFun/Searches/SearchMechanics.java
@@ -0,0 +1,63 @@
+package searchingForFun.Searches;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import rp13.search.problem.puzzle.EightPuzzle;
+import rp13.search.problem.puzzle.EightPuzzleSuccessorFunction;
+import rp13.search.problem.puzzle.EightPuzzle.PuzzleMove;
+import rp13.search.util.ActionStatePair;
+
+public class SearchMechanics {
+
+	private QueueClass<ActionStatePair<PuzzleMove, EightPuzzle>> agenda;
+	private EightPuzzle start;
+	private EightPuzzleSuccessorFunction sf = new EightPuzzleSuccessorFunction();
+	private ActionStatePair<PuzzleMove, EightPuzzle> node;
+
+	public ActionStatePair<PuzzleMove, EightPuzzle> Search() {
+
+		agenda = new QueueClass<ActionStatePair<PuzzleMove, EightPuzzle>>();
+		start = EightPuzzle.randomEightPuzzle();
+
+		System.out.println(start);
+
+		EightPuzzle goal = EightPuzzle.orderedEightPuzzle();
+
+		System.out.println(goal);
+
+		// start.getSuccessor(start,list);
+		Queue<EightPuzzle> visited = new LinkedList<EightPuzzle>();
+		List<ActionStatePair<PuzzleMove, EightPuzzle>> _successors = new ArrayList<ActionStatePair<PuzzleMove, EightPuzzle>>();
+		sf.getSuccessors(start, _successors);
+
+		visited.add(start);
+
+		for (ActionStatePair<PuzzleMove, EightPuzzle> node : _successors) {
+			// System.out.println(node);
+			if (visited.contains(node.getState()) == false)
+				agenda.push(node);
+
+		}
+		_successors.clear();
+		while (!agenda.isEmpty()) {
+			node = agenda.pop();
+			// System.out.println(node);
+			if (node.getState().equals(goal)) {
+
+				return node;
+			} else {
+				sf.getSuccessors(node.getState(), _successors);
+				visited.add(node.getState());
+				for (ActionStatePair<PuzzleMove, EightPuzzle> suc : _successors) {
+					if (visited.contains(suc.getState()) == false)
+						agenda.push(suc);
+				}
+
+			}
+			_successors.clear();
+		}
+		return node;
+	}
+}
