b75f7028d550e166b0bdf4b19ef1107ec6c7cee6
Jugganourt <t.sammons@hotmail.com>
2014-03-04 04:30:46 +0000

    removeal of git hub inputs

--- SearchingForFun_PC/src/searchingForFun/Searches/SearchObject.java
+++ SearchingForFun_PC/src/searchingForFun/Searches/SearchObject.java
@@ -23,7 +23,6 @@ public class SearchObject {
 //		Agenda<ActionStatePair<PuzzleMove, EightPuzzle>> agenda2 = new StackClass<ActionStatePair<PuzzleMove, EightPuzzle>>();
 	
 		/** BFS PUZZLE
-=======
 		
 		SearchMechanics<PuzzleMove, EightPuzzle > sm = new SearchMechanics<>();
 		
@@ -33,7 +32,6 @@ public class SearchObject {
 		Agenda<ActionStatePair<PuzzleMove, EightPuzzle>> agenda2 = new StackClass<ActionStatePair<PuzzleMove, EightPuzzle>>();
 		System.out.println("goal:");
 		System.out.println(star.doSearch(EightPuzzle.randomEightPuzzle(),EightPuzzle.orderedEightPuzzle(), sf, agenda2));
->>>>>>> 0c99286fad7bd1565d7a4edd3933a5227eae4321
 		Stack<ActionStatePair<PuzzleMove,EightPuzzle>> s = sm.doSearch(EightPuzzle.randomEightPuzzle(),EightPuzzle.orderedEightPuzzle(), sf, agenda);
 		
 		for(int i = s.size()-1 ; i >= 0; i--){
