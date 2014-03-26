e9578cb516ac88916355808473f6e4b732fc131d
Jugganourt <t.sammons@hotmail.com>
2014-03-26 23:24:12 +0000

    Grid methods move about

--- Labyrinth_PC/src/robot/moves/Grid2.java
+++ Labyrinth_PC/src/robot/moves/Grid2.java
@@ -1,7 +1,10 @@
 package robot.moves;
 
 import java.util.Random;
-
+import lejos.nxt.Button;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.OpticalDistanceSensor;
 import lejos.util.Delay;
 import rp.robotics.localisation.ActionModel;
 import rp.robotics.localisation.GridPositionDistribution;
@@ -10,8 +13,16 @@ import rp.robotics.mapping.GridMap;
 import rp.robotics.mapping.Heading;
 import rp.robotics.mapping.LocalisationUtils;
 
-public class Grid2 extends RobotMoves {
-
+public class Grid2 extends RobotSettings {
+	
+	public static LightSensor s_left = new LightSensor(SensorPort.S3);
+	public static LightSensor s_right = new LightSensor(SensorPort.S2);
+	private int threshold = 40;
+	private static OpticalDistanceSensor sensor = new OpticalDistanceSensor(SensorPort.S4);
+	private int wall = 300;
+	private int noLine = 15;
+	private int shortDistance = 5;
+	PerfectActionModel model = new PerfectActionModel();
 	private Random rand = new Random();
 
 	public Grid2() {
@@ -63,6 +74,85 @@ public class Grid2 extends RobotMoves {
 		buttonPress();
 		Grid2 run = new Grid2();
 		run.run();		
+		
+	}
+	public static void buttonPress(){
+		Button.waitForAnyPress();
+		Delay.msDelay(500);
+	}
+	
+	public void leftCondition(){
+		if(s_left.readValue() < threshold && s_right.readValue() >threshold)
+		{
+			pilot.rotate(noLine);
+			pilot.travel(shortDistance);
+		}
+	}
+	
+	public void rightCondition()
+	{
+		if(s_left.readValue() > threshold && s_right.readValue() < threshold)
+		{
+			pilot.rotate(-1*noLine);
+			pilot.travel(shortDistance);
+		}
+	}
+	
+	public void forwardCondition(){
+		if(s_left.readValue() > threshold && s_right.readValue() > threshold)
+		{
+			pilot.forward(); 
+		} 
+	}
+	
+	public void closeToGridWallCondition(){
+		if(sensor.getDistance() < wall){
+			
+			pilot.stop();
+			
+			pilot.rotate(90);
+		}
+	}
+	
+	private boolean canMove() {
+		if(sensor.getDistance() > wall){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public void junction(int ran){
+		
+		if(s_left.readValue() < threshold && s_right.readValue() < threshold)
+		{
+			
+			pilot.travel(60);
+			if(ran == 1){
+				pilot.rotate(90);
+			}
+			else if(ran ==2){
+					pilot.rotate(-90);
+			}
+			else{ pilot.forward();}
+			while(canMove()){
+				closeToGridWallCondition();
+			}	
+			
+		}
+		
+	
+	}
+	
+	protected boolean junction() {
+		if(s_left.readValue() < threshold && s_right.readValue() < threshold)
+		{
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 }
