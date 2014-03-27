9b42644cb5c89e6dd1581767250522f75a5f44d8
Jugganourt <t.sammons@hotmail.com>
2014-03-27 01:47:43 +0000

    Well it localised onced

--- Labyrinth_PC/src/robot/moves/Grid2.java
+++ Labyrinth_PC/src/robot/moves/Grid2.java
@@ -24,6 +24,17 @@ public class Grid2 extends RobotSettings {
 	private int shortDistance = 5;
 	PerfectActionModel model = new PerfectActionModel();
 	private Random rand = new Random();
+	
+	
+	private GridMap gridMap = LocalisationUtils.create2014Map1();
+	private GridPositionDistribution distribution = new GridPositionDistribution(
+			gridMap);
+	private ActionModel actionModel = new PerfectActionModel();
+	private Heading action = Heading.PLUS_X;
+	private float max = 0;
+	private int xMax = 0;
+	private int yMax = 0;
+	private double thres = 0.1;
 
 	public Grid2() {
 		super();
@@ -32,42 +43,15 @@ public class Grid2 extends RobotSettings {
 	
 	
 	public void run() {
-		GridMap gridMap = LocalisationUtils.create2014Map1();
-
-		// The probability distribution over the robot's location
-		GridPositionDistribution distribution = new GridPositionDistribution(
-				gridMap);
-
-
-		// ActionModel actionModel = new DummyActionModel();
-		ActionModel actionModel = new PerfectActionModel();
-		while (m_run()) {
+			while (m_run()) {
 			pilot.setTravelSpeed(100);
 			
-			
-
-			
+			int ran = rand.nextInt(3);			
 			forwardCondition();
 			rightCondition();
 			leftCondition();
-			
-			//while (true) {
-				if(junction()){
-					pilot.stop();
-				int ran = rand.nextInt(3);
-
-				junction(ran);				
-				
-				Heading action = Heading.PLUS_X;
-				System.out.println(action);
-				distribution = actionModel.updateAfterMove(distribution, action);
-				distribution.normalise();
-
-				System.out.println("map sum: " + distribution.sumProbabilities());
-
-			}
+			junction(ran);	
 		}
-
 	}
 
 	public static void main(String[] args) {
@@ -125,34 +109,128 @@ public class Grid2 extends RobotSettings {
 	
 	public void junction(int ran){
 		
+		//itterate through the map
+		//get the maximum probability in the map
+		//in the while true 
+		// while max< thresh
+		
+		if(maxprob(gridMap)< thres){
+			
 		if(s_left.readValue() < threshold && s_right.readValue() < threshold)
 		{
 			
 			pilot.travel(60);
 			if(ran == 1){
+				System.out.println(action);
+				distribution = actionModel.updateAfterMove(distribution, action);
+				distribution.normalise();
+				action = cycleThroughtLeft(action);
 				pilot.rotate(90);
+				
+				
 			}
 			else if(ran ==2){
+				System.out.println(action);
+				distribution = actionModel.updateAfterMove(distribution, action);
+				distribution.normalise();
+				action = cycleThroughtRight(action);
 					pilot.rotate(-90);
 			}
-			else{ pilot.forward();}
+			else{ 
+				System.out.println(action);
+				distribution = actionModel.updateAfterMove(distribution, action);
+				distribution.normalise();
+				action = cycleThroughtForward(action);
+				pilot.forward();
+				}
+			
 			while(canMove()){
 				closeToGridWallCondition();
-			}	
+				action = cycleThroughtLeft(action);
+			}
+			
+
+
+			System.out.println("map sum: " + distribution.sumProbabilities());
 			
 		}
+		}
+		if(maxprob(gridMap)>thres){
+		System.out.println("Xposs:" + xMax);
+		System.out.println("Xposs:" + yMax);
+		}
 		
 	
 	}
 	
-	protected boolean junction() {
-		if(s_left.readValue() < threshold && s_right.readValue() < threshold)
-		{
-			return true;
+	private double maxprob(GridMap gridMap) {
+		
+	
+		for (int y = 0; y < gridMap.getGridHeight(); y++) {
+			for (int x = 0; x < gridMap.getGridWidth(); x++) 
+				if(distribution.getProbability(x, y)>max)
+				{
+					max = distribution.getProbability(x, y);
+					xMax = x;
+					yMax = y;
+				}
 		}
-		else{
-			return false;
-		}
+		System.out.println("maximum "+max);
+		return max;
 		
 	}
+
+
+
+	public Heading cycleThroughtLeft(Heading action){
+		if(action == Heading.PLUS_X ){
+			 action = Heading.MINUS_Y;
+		}
+		else if( action == Heading.MINUS_Y){
+			action = Heading.MINUS_X;
+		}
+		else if(action == Heading.MINUS_X){
+			action = Heading.PLUS_Y;
+		}
+		else if(action == Heading.PLUS_Y){
+			action = Heading.PLUS_X;
+		}
+		return action;
+		
+	}	
+	
+	public Heading cycleThroughtRight(Heading action){
+		if(action == Heading.PLUS_X ){
+			 action = Heading.PLUS_Y;
+		}
+		else if( action == Heading.PLUS_Y){
+			action = Heading.MINUS_X;
+		}
+		else if(action == Heading.MINUS_X){
+			action = Heading.MINUS_Y;
+		}
+		else if(action == Heading.MINUS_Y){
+			action = Heading.PLUS_X;
+		}
+		return action;
+		
+	}	
+	
+	public Heading cycleThroughtForward(Heading action){
+		if(action == Heading.PLUS_X ){
+			 action = Heading.PLUS_X;
+		}
+		else if( action == Heading.PLUS_Y){
+			action = Heading.PLUS_Y;
+		}
+		else if(action == Heading.MINUS_X){
+			action = Heading.MINUS_X;
+		}
+		else if(action == Heading.MINUS_Y){
+			action = Heading.MINUS_Y;
+		}
+		return action;
+		
+	}	
+	
 }
