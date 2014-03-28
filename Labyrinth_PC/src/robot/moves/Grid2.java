8e3d47c5cba38f1dcd37464bbfc84780234167c3
Jugganourt <t.sammons@hotmail.com>
2014-03-28 03:33:49 +0000

    tom's night updates

--- Labyrinth_PC/src/robot/moves/Grid2.java
+++ Labyrinth_PC/src/robot/moves/Grid2.java
@@ -1,17 +1,23 @@
 package robot.moves;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-import data.Node;
-import data.Point;
-import data_structures.Stack;
+import Grid.ForbiddenCollection;
+import Grid.ForbiddenRoute;
+import Grid.Grid;
+import Grid.GridSuccessorFunction;
+import Grid.Grid.PuzzleMove;
+import action.AStarAgenda;
+import action.AstarSearch;
+import action.Node;
 import lejos.nxt.Button;
 import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.Sound;
 import lejos.nxt.addon.OpticalDistanceSensor;
 import lejos.util.Delay;
-import main.SolveGrid;
 import rp.robotics.localisation.ActionModel;
 import rp.robotics.localisation.GridPositionDistribution;
 import rp.robotics.localisation.PerfectActionModel;
@@ -19,22 +25,20 @@ import rp.robotics.localisation.PerfectSensorModel;
 import rp.robotics.mapping.GridMap;
 import rp.robotics.mapping.Heading;
 import rp.robotics.mapping.LocalisationUtils;
-import search.Solve;
-import search.Solve.Search;
 
 public class Grid2 extends RobotSettings {
-	
+
 	public static LightSensor s_left = new LightSensor(SensorPort.S3);
 	public static LightSensor s_right = new LightSensor(SensorPort.S2);
 	private int threshold = 40;
-	private static OpticalDistanceSensor sensor = new OpticalDistanceSensor(SensorPort.S4);
+	private static OpticalDistanceSensor sensor = new OpticalDistanceSensor(
+			SensorPort.S4);
 	private int wall = 315;
 	private int noLine = 15;
 	private int shortDistance = 5;
 	PerfectActionModel model = new PerfectActionModel();
 	private Random rand = new Random();
-	
-	
+
 	private GridMap gridMap = LocalisationUtils.create2014Map1();
 	private GridPositionDistribution distribution = new GridPositionDistribution(
 			gridMap);
@@ -49,266 +53,379 @@ public class Grid2 extends RobotSettings {
 		super();
 	}
 
-	
-	
 	public void run() {
-			while (m_run()) {
+		while (m_run()) {
 			pilot.setTravelSpeed(100);
-			
-			int ran = rand.nextInt(3);			
+			FindAPlace(0, 0);
+			/*int ran = rand.nextInt(3);
 			forwardCondition();
 			rightCondition();
 			leftCondition();
-			junction(ran);	
+			junction(ran);*/
 		}
 	}
 
 	public static void main(String[] args) {
 		buttonPress();
 		Grid2 run = new Grid2();
-		run.run();		
-		
+		run.run();
+
 	}
-	public static void buttonPress(){
+
+	public static void buttonPress() {
 		Button.waitForAnyPress();
 		Delay.msDelay(500);
 	}
-	
-	public void leftCondition(){
-		if(s_left.readValue() < threshold && s_right.readValue() >threshold)
-		{
+
+	public void leftCondition() {
+		if (s_left.readValue() < threshold && s_right.readValue() > threshold) {
 			pilot.rotate(noLine);
 			pilot.travel(shortDistance);
 		}
 	}
-	
-	public void rightCondition()
-	{
-		if(s_left.readValue() > threshold && s_right.readValue() < threshold)
-		{
-			pilot.rotate(-1*noLine);
+
+	public void rightCondition() {
+		if (s_left.readValue() > threshold && s_right.readValue() < threshold) {
+			pilot.rotate(-1 * noLine);
 			pilot.travel(shortDistance);
 		}
 	}
-	
-	public void forwardCondition(){
-		if(s_left.readValue() > threshold && s_right.readValue() > threshold)
-		{
-			pilot.forward(); 
-		} 
+
+	public void forwardCondition() {
+		if (s_left.readValue() > threshold && s_right.readValue() > threshold) {
+			pilot.forward();
+		}
 	}
-	
-	public void closeToGridWallCondition(){
-		if(sensor.getDistance() < wall){
-			
+
+	public void closeToGridWallCondition() {
+		if (sensor.getDistance() < wall) {
+
 			pilot.stop();
-			
+
 			pilot.rotate(90);
 		}
 	}
-	
+
 	private boolean canMove() {
-		if(sensor.getDistance() > wall){
+		if (sensor.getDistance() > wall) {
 			return false;
-		}
-		else{
+		} else {
 			return true;
 		}
 	}
-	
-	public void junction(int ran){
-		
-		//itterate through the map
-		//get the maximum probability in the map
-		//in the while true 
+
+	public void junction(int ran) {
+
+		// itterate through the map
+		// get the maximum probability in the map
+		// in the while true
 		// while max< thresh
-		
-		if(maxprob(gridMap)< thres){
-			
-			if(s_left.readValue() < threshold && s_right.readValue() < threshold)
-			{
-			
+
+		if (maxprob(gridMap) < thres) {
+
+			if (s_left.readValue() < threshold
+					&& s_right.readValue() < threshold) {
+
 				pilot.travel(60);
-				/*if(ran == 1){
+				/*
+				 * if(ran == 1){ System.out.println(action); distribution =
+				 * actionModel.updateAfterMove(distribution, action);
+				 * distribution.normalise(); action = cycleThroughtLeft(action);
+				 * //distribution = sensing4D(distribution); //sensing //
+				 * senseleft, right, ahead, update distribution and carry on
+				 * 
+				 * pilot.rotate(90);
+				 * 
+				 * 
+				 * } else if(ran ==2){ System.out.println(action); distribution
+				 * = actionModel.updateAfterMove(distribution, action);
+				 * distribution.normalise(); action =
+				 * cycleThroughtRight(action); //distribution =
+				 * sensing4D(distribution); pilot.rotate(-90); } else{
+				 * System.out.println(action); distribution =
+				 * actionModel.updateAfterMove(distribution, action);
+				 * distribution.normalise(); action =
+				 * cycleThroughtForward(action); //distribution =
+				 * sensing4D(distribution); pilot.forward(); }
+				 * 
+				 * while(canMove()){ closeToGridWallCondition(); action =
+				 * cycleThroughtLeft(action); }
+				 */
+
+				if (sensor.getDistance() > wall) {
 					System.out.println(action);
-					distribution = actionModel.updateAfterMove(distribution, action);
-					distribution.normalise();
-					action = cycleThroughtLeft(action);
-					//distribution = sensing4D(distribution);
-					//sensing
-					// senseleft, right, ahead, update distribution and carry on 
-				
-					pilot.rotate(90);
-				
-				
-				}
-				else if(ran ==2){
-					System.out.println(action);
-					distribution = actionModel.updateAfterMove(distribution, action);
-					distribution.normalise();
-					action = cycleThroughtRight(action);
-					//distribution = sensing4D(distribution);
-					pilot.rotate(-90);
-				}
-				else{ 
-					System.out.println(action);
-					distribution = actionModel.updateAfterMove(distribution, action);
-					distribution.normalise();
-					action = cycleThroughtForward(action);
-					//distribution = sensing4D(distribution);
-					pilot.forward();
-				}
-			
-				while(canMove()){
-					closeToGridWallCondition();
-					action = cycleThroughtLeft(action);			
-				}*/
-				
-				if(sensor.getDistance() > wall){
-					System.out.println(action);
-					distribution = actionModel.updateAfterMove(distribution, action);
+					distribution = actionModel.updateAfterMove(distribution,
+							action);
 					distribution.normalise();
 					action = cycleThroughtForward(action);
-				}
-				else{
+				} else {
 					System.out.println(action);
-					distribution = actionModel.updateAfterMove(distribution, action);
+					distribution = actionModel.updateAfterMove(distribution,
+							action);
 					distribution.normalise();
-					while(canMove()){
+					while (canMove()) {
 						closeToGridWallCondition();
-						action = cycleThroughtLeft(action);			
+						action = cycleThroughtLeft(action);
 					}
 				}
-				System.out.println("map sum: " + distribution.sumProbabilities());
+				System.out.println("map sum: "
+						+ distribution.sumProbabilities());
 			}
 		}
-		if(maxprob(gridMap)>thres){
-			
+		if (maxprob(gridMap) > thres) {
+
 			Sound.beep();
 			System.out.println("Xposs:" + xMax);
 			System.out.println("Yposs:" + yMax);
 			FindAPlace(xMax, yMax);
-		
+
 		}
-		
-	
+
 	}
-	
+
 	private void FindAPlace(int xPoss, int yPoss) {
-			
-			Sound.beepSequence();
-		
-			Stack<Node<Point>> nodes = Solve.solveGrid(new Point(xPoss,yPoss,(xPoss+yPoss)), new Point(5,3,(5+3)), Search.BREADTH_FIRST);
-			SolveGrid grid = new SolveGrid(xPoss, yPoss, null);
-			
+
+		Sound.beepSequence();
+
+		List<ForbiddenRoute> foblist = new ArrayList<ForbiddenRoute>();
+
+		foblist.add(new ForbiddenRoute(2,13));
+		foblist.add(new ForbiddenRoute(13,14));
+		foblist.add(new ForbiddenRoute(13,24));
+		foblist.add(new ForbiddenRoute(23,24));
+		foblist.add(new ForbiddenRoute(23,34));
+		foblist.add(new ForbiddenRoute(22,23));
+		foblist.add(new ForbiddenRoute(4,15));
+		foblist.add(new ForbiddenRoute(5,16));
+		foblist.add(new ForbiddenRoute(6,17));
+		foblist.add(new ForbiddenRoute(15,26));
+		foblist.add(new ForbiddenRoute(16,27));
+		foblist.add(new ForbiddenRoute(17,28));
+		foblist.add(new ForbiddenRoute(8,19));
+		foblist.add(new ForbiddenRoute(18,19));
+		foblist.add(new ForbiddenRoute(19,30));
+		foblist.add(new ForbiddenRoute(30,31));
+		foblist.add(new ForbiddenRoute(31,42));
+		foblist.add(new ForbiddenRoute(31,32));
+		foblist.add(new ForbiddenRoute(37,38));
+		foblist.add(new ForbiddenRoute(38,49));
+		foblist.add(new ForbiddenRoute(38,39));
+		foblist.add(new ForbiddenRoute(44,45));
+		foblist.add(new ForbiddenRoute(34,45));
+		foblist.add(new ForbiddenRoute(45,46));
+		foblist.add(new ForbiddenRoute(46,57));
+		foblist.add(new ForbiddenRoute(57,58));
+		foblist.add(new ForbiddenRoute(57,68));
+		foblist.add(new ForbiddenRoute(48,59));
+		foblist.add(new ForbiddenRoute(49,60));
+		foblist.add(new ForbiddenRoute(50,61));
+		foblist.add(new ForbiddenRoute(59,70));
+		foblist.add(new ForbiddenRoute(60,71));
+		foblist.add(new ForbiddenRoute(61,72));
+		foblist.add(new ForbiddenRoute(52,53));
+		foblist.add(new ForbiddenRoute(42,53));
+		foblist.add(new ForbiddenRoute(52,63));
+		foblist.add(new ForbiddenRoute(62,63));
+		foblist.add(new ForbiddenRoute(63,74));
+		foblist.add(new ForbiddenRoute(53,54));
+
+
+		ForbiddenCollection fob = new ForbiddenCollection(foblist);
+
+		Grid state = new Grid(xMax, yMax, fob);
+
+		Grid goal = new Grid(5, 3, fob);
+
+		GridSuccessorFunction successor = new GridSuccessorFunction();
+
+		AStarAgenda<Node<PuzzleMove, Grid>> agenda = new AStarAgenda<Node<PuzzleMove, Grid>>();
+
+		AstarSearch<PuzzleMove, Grid> ass = new AstarSearch<PuzzleMove, Grid>(
+				state, goal, successor, agenda);
+
+		Node node = ass.doSearch();
+
+		List<PuzzleMove> path = new ArrayList<PuzzleMove>();
+
+		path = node.getPath();
+
+		System.out.println(path);
+		int count = 0;
+		// Stack<Node<Point>> nodes = Solve.solveGrid(new
+		// Point(xPoss,yPoss,(xPoss+yPoss)), new Point(5,3,(5+3)),
+		// Search.BREADTH_FIRST);
+		// SolveGrid grid = new SolveGrid(xPoss, yPoss, null);
+		while (true) {
+			forwardCondition();
+			rightCondition();
+			leftCondition();
+			if (s_left.readValue() < threshold && s_right.readValue() < threshold) {
+				pilot.travel(60);
+				PuzzleMove rount = path.get(count);
+				System.out.println(count);
+				System.out.println(action);
+				System.out.println(rount.toString());
+				System.out.println(rount);
+				if (rount.toString().equals("UP")) {
+					if(action == Heading.PLUS_Y){
+						cycleThroughtForward(action);
+					}
+					else if(action == Heading.PLUS_X){
+						pilot.rotate(90);
+						cycleThroughtLeft(action);
+					}
+					else if(action == Heading.MINUS_X){
+						pilot.rotate(-90);
+						cycleThroughtRight(action);
+					}
+					else if(action == Heading.MINUS_Y){
+						pilot.rotate(180);
+						cycleThroughtBackwards(action);
+					}
+					
+				} else if (rount.toString().equals("DOWN")) {
+					if(action == Heading.PLUS_Y){
+						pilot.rotate(180);
+						cycleThroughtBackwards(action);
+					}
+					else if(action == Heading.PLUS_X){
+						pilot.rotate(-90);
+						cycleThroughtRight(action);
+					}
+					else if(action == Heading.MINUS_X){
+						pilot.rotate(90);
+						cycleThroughtLeft(action);
+					}
+					else if(action == Heading.MINUS_Y){
+						cycleThroughtForward(action);
+					}
+					
+				} else if (rount.toString().equals("LEFT")) {
+					if(action == Heading.PLUS_Y){
+						pilot.rotate(-90);						
+						cycleThroughtRight(action);
+					}
+					else if(action == Heading.PLUS_X){
+						pilot.rotate(180);
+						cycleThroughtBackwards(action);
+					}
+					else if(action == Heading.MINUS_X){
+						cycleThroughtForward(action);
+					}
+					else if(action == Heading.MINUS_Y){
+						pilot.rotate(90);
+						cycleThroughtLeft(action);
+					}
+				} else if (rount.toString().equals("RIGHT")) {
+					if(action == Heading.PLUS_Y){
+						pilot.rotate(90);
+						cycleThroughtLeft(action);
+					}
+					else if(action == Heading.PLUS_X){
+						cycleThroughtForward(action);
+					}
+					else if(action == Heading.MINUS_X){
+						pilot.rotate(180);
+						cycleThroughtBackwards(action);
+					}
+					else if(action == Heading.MINUS_Y){
+						pilot.rotate(-90);
+						cycleThroughtRight(action);
+					}
+					else{
+						System.out.println("WELL SHIT!");
+					}
+				}
+				if (count != path.size() - 1) {
+					System.out.println(path.get(count));
+					count++;
+				}
+				
+			}
+		}
 	}
-
-	
-
 
 	private double maxprob(GridMap gridMap) {
-		
-	
+
 		for (int y = 0; y < gridMap.getGridHeight(); y++) {
-			for (int x = 0; x < gridMap.getGridWidth(); x++) 
-				if(distribution.getProbability(x, y)>max)
-				{
+			for (int x = 0; x < gridMap.getGridWidth(); x++)
+				if (distribution.getProbability(x, y) > max) {
 					max = distribution.getProbability(x, y);
 					xMax = x;
 					yMax = y;
 				}
 		}
-		System.out.println("maximum "+max);
+		System.out.println("maximum " + max);
 		return max;
-		
+
 	}
 
+	public static Heading cycleThroughtLeft(Heading action) {
+		if (action == Heading.PLUS_X) {
+			action = Heading.MINUS_Y;
+		} else if (action == Heading.MINUS_Y) {
+			action = Heading.MINUS_X;
+		} else if (action == Heading.MINUS_X) {
+			action = Heading.PLUS_Y;
+		} else if (action == Heading.PLUS_Y) {
+			action = Heading.PLUS_X;
+		}
+		return action;
 
+	}
 
-	public static Heading cycleThroughtLeft(Heading action){
-		if(action == Heading.PLUS_X ){
-			 action = Heading.MINUS_Y;
-		}
-		else if( action == Heading.MINUS_Y){
-			action = Heading.MINUS_X;
-		}
-		else if(action == Heading.MINUS_X){
+	public static Heading cycleThroughtRight(Heading action) {
+		if (action == Heading.PLUS_X) {
 			action = Heading.PLUS_Y;
-		}
-		else if(action == Heading.PLUS_Y){
+		} else if (action == Heading.PLUS_Y) {
+			action = Heading.MINUS_X;
+		} else if (action == Heading.MINUS_X) {
+			action = Heading.MINUS_Y;
+		} else if (action == Heading.MINUS_Y) {
 			action = Heading.PLUS_X;
 		}
 		return action;
-		
-	}	
-	
-	public static Heading cycleThroughtRight(Heading action){
-		if(action == Heading.PLUS_X ){
-			 action = Heading.PLUS_Y;
-		}
-		else if( action == Heading.PLUS_Y){
-			action = Heading.MINUS_X;
-		}
-		else if(action == Heading.MINUS_X){
-			action = Heading.MINUS_Y;
-		}
-		else if(action == Heading.MINUS_Y){
+
+	}
+
+	public static Heading cycleThroughtForward(Heading action) {
+		if (action == Heading.PLUS_X) {
 			action = Heading.PLUS_X;
-		}
-		return action;
-		
-	}	
-	
-	public static Heading cycleThroughtForward(Heading action){
-		if(action == Heading.PLUS_X ){
-			 action = Heading.PLUS_X;
-		}
-		else if( action == Heading.PLUS_Y){
+		} else if (action == Heading.PLUS_Y) {
 			action = Heading.PLUS_Y;
-		}
-		else if(action == Heading.MINUS_X){
+		} else if (action == Heading.MINUS_X) {
 			action = Heading.MINUS_X;
-		}
-		else if(action == Heading.MINUS_Y){
+		} else if (action == Heading.MINUS_Y) {
 			action = Heading.MINUS_Y;
 		}
 		return action;
-		
-	}	
-	
-	public static Heading cycleThroughtBackwards(Heading action){
-		if(action == Heading.PLUS_X ){
-			 action = Heading.MINUS_X;
-		}
-		else if( action == Heading.PLUS_Y){
+
+	}
+
+	public static Heading cycleThroughtBackwards(Heading action) {
+		if (action == Heading.PLUS_X) {
+			action = Heading.MINUS_X;
+		} else if (action == Heading.PLUS_Y) {
 			action = Heading.MINUS_Y;
-		}
-		else if(action == Heading.MINUS_X){
+		} else if (action == Heading.MINUS_X) {
 			action = Heading.PLUS_X;
-		}
-		else if(action == Heading.MINUS_Y){
+		} else if (action == Heading.MINUS_Y) {
 			action = Heading.PLUS_Y;
 		}
 		return action;
-		
-	}	
-	/**public GridPositionDistribution sensing4D(GridPositionDistribution g1){
-		
-		PerfectSensorModel sensorModel = new PerfectSensorModel(action);
-		float valueF = sensor.getRange();
-		sensormotor.rotate(95);
-		System.out.println(valueF);
-		float valueR = sensor.getRange();
-		sensormotor.rotate(95);
-		System.out.println(valueR);
-		float valueB = sensor.getRange();
-		sensormotor.rotate(95);
-		System.out.println(valueB);
-		float valueL = sensor.getRange();
-		System.out.println(valueL);
-		sensormotor.rotate(-285);
-		g1 = sensorModel.updateDistributionAfterSensing(valueL,valueR, valueF, valueB,g1);
-		g1.normalise();
-		return g1;
-	}**/
+
+	}
+	/**
+	 * public GridPositionDistribution sensing4D(GridPositionDistribution g1){
+	 * 
+	 * PerfectSensorModel sensorModel = new PerfectSensorModel(action); float
+	 * valueF = sensor.getRange(); sensormotor.rotate(95);
+	 * System.out.println(valueF); float valueR = sensor.getRange();
+	 * sensormotor.rotate(95); System.out.println(valueR); float valueB =
+	 * sensor.getRange(); sensormotor.rotate(95); System.out.println(valueB);
+	 * float valueL = sensor.getRange(); System.out.println(valueL);
+	 * sensormotor.rotate(-285); g1 =
+	 * sensorModel.updateDistributionAfterSensing(valueL,valueR, valueF,
+	 * valueB,g1); g1.normalise(); return g1; }
+	 **/
 }
